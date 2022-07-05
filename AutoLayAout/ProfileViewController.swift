07b2adc0df7062a664ef57ced41be2e59cf0d579
mirian-m <mirianmaglakelidze7@gmail.com>
2022-07-05 15:19:01 +0400

    Add AutoLayaut project

---
+++ AutoLayAout/ProfileViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var save: UIButton! {
+        didSet {
+            save.layer.cornerRadius = 20
+        }
+    }
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var location: UITextField!
+    
+    lazy var textFieldArray: [UITextField] = [userName, password, email,location]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTextfieldDesign()
+    }
+    
+    func setTextfieldDesign() {
+        textFieldArray.forEach { $0.underlined(color: .systemGray) }
+    }
+}
+
+extension UITextField {
+    func underlined(color: UIColor) {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = color.cgColor
+
+        /* შევქმნათ მონაკვეთი რომლის საწყისი წერტილი იქნება (0, self.frame.size.height - width)
+         ხოლო ბოლო წერტილი (self.frame.size.width, height: self.frame.size.height) */
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+
+        border.borderWidth = width
+        // დავამატოთ TextField ის leyer შექმნილი მონაკვეთი (border)
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+    
+
+}
--- AutoLayAout/ViewController.swift
+++ AutoLayAout/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUp: UIButton! {
+        didSet {
+            signUp.layer.cornerRadius = 10
+            signUp.backgroundColor = .systemBlue
+            signUp.setTitleColor(.black, for: .normal)
+        }
+    }
+    @IBOutlet weak var logIN: UIButton! {
+        didSet {
+            logIN.layer.cornerRadius = 10
+            logIN.backgroundColor = .white
+            logIN.setTitleColor(.black, for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
